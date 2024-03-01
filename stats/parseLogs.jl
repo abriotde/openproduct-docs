322f1c0402ae9ab77f27d1acb23f4aa6c228440e
alberic <alberic.briot@osiris-team.fr>
2024-03-02 00:54:07 +0100

    FIX parseLogs.jl

--- stats/parseLogs.jl
+++ stats/parseLogs.jl
@@ -9,8 +9,8 @@ using GZip
 DATA_PATH = "./data/"
 
 regexLogFile = Regex("addon-openproduct.fr.kaja9241.odns.fr(-ssl_log)?-([A-Za-z]+)-([0-9]+).gz")
-regexLogLine = Regex("(.*) -.*- \\[([0-9]+)/.*\\].*")
-MonthTranslation = Dict(
+regexLogLine = Regex("(.*) -.*- \\[([0-9]+)/([A-Z][a-z]+)/([0-9]+).*\\].*")
+MonthTranslation = Dict{String, Int}(
 	"Jan" => 1,
 	"Feb" => 2,
 	"Mar" => 3,
@@ -25,6 +25,7 @@ MonthTranslation = Dict(
 	"Dec" => 12
 )
 
+monthVisitors = Dict{Integer,Dict{String, Bool}}()
 #=
 	Get log files from webserver
 =#
@@ -38,11 +39,9 @@ end
 #=
 	Parse log file `filename` and generate a Dict with use statistics 
 =#
-function getStatsFromFile(filename)
-	stats = Dict()
-	dateRef = nothing
+function getStatsFromFile(filename, stats)
+	visitors = dateRef = monthStr = yearStr = month_i = month_i = nothing
 	fd = GZip.open(DATA_PATH*"/"*filename, "r") do io
-		visitors = Dict()
 		linenb = 0
 		while (line = readline(io))!=""
 			# println("Line:",line)
@@ -53,19 +52,52 @@ function getStatsFromFile(filename)
 				ip = m[1]
 				date = m[2]
 				if dateRef==nothing
+					# Init for next dates
+					visitors = Dict{String, Bool}()
 					dateRef = date
-					# println("Date:",date)
+					monthStr = m[3]
+					yearStr = m[4]
+					month_i = MonthTranslation[monthStr]
+					if !haskey(monthVisitors, month_i)
+						monthVisitors[month_i] = Dict{String, Bool}()
+					end
 				elseif dateRef!=date
-					# println("Date change:",date)
+					# Save values
+					date_i = parse(Int64, dateRef)
+					if !haskey(stats, month_i)
+						stats[month_i] = Dict{Int, Int}()
+					end
 					val = length(visitors)
-					date_i = parse(Int64, dateRef);
-					stats[date_i] = val
+					if haskey(stats[month_i], date_i)
+						val += stats[month_i][date_i]
+					end
+					stats[month_i][date_i] = val
+					# Init for next dates
+					visitors = Dict{String, Bool}()
 					dateRef = date
-					visitors = Dict()
+					monthStr = m[3]
+					yearStr = m[4]
+					month_i = MonthTranslation[monthStr]
+					if !haskey(monthVisitors, month_i)
+						monthVisitors[month_i] = Dict{String, Bool}()
+					end
 				end
-				visitors[ip] = true;
+				# print(".")
+				visitors[ip] = true
+				monthVisitors[month_i][ip] = true
 			end
 		end
+		# Save values
+		date_i = parse(Int64, dateRef);
+		if !haskey(stats, month_i)
+			stats[month_i] = Dict{Int, Int}()
+		end
+		val = length(visitors)
+		if haskey(stats[month_i], date_i)
+			val += stats[month_i][date_i]
+		end
+		stats[month_i][date_i] = val
+		# println()
 	end
 	stats
 end
@@ -74,7 +106,7 @@ end
 	Parse all log files in `DATA_PATH` and return  a Dict with use statistics 
 =#
 function getStats()
-	stats = Dict()
+	stats = Dict{Int,Dict{Int, Int}}()
 	for filename in readdir(DATA_PATH)
 		println("File : ",filename)
 		m=match(regexLogFile, filename)
@@ -82,26 +114,8 @@ function getStats()
 			month = m[2]
 			global year = m[3]
 			# println("Year:",year)
-			month_i = MonthTranslation[month]
-			monthStats = getStatsFromFile(filename)
-			if !haskey(stats, month_i)
-				stats[month_i] = monthStats
-			else
-				# Concatenate results of multi-files (https && http)
-				monthStats0 = stats[month_i]
-				for day in 1:31
-					val = 0
-					if haskey(monthStats, day)
-						val = monthStats[day]
-					end
-					if haskey(monthStats0, day)
-						val += monthStats0[day]
-					end
-					if val>0
-						stats[month_i][day] = val
-					end
-				end
-			end
+			getStatsFromFile(filename, stats)
+			# println("stats : ",stats)
 		else
 			println("Error : file ",filename," not match")
 		end
@@ -122,11 +136,12 @@ function printStats(stats)
 					println(day,"/",month,"/",year," => ",monthStats[day])
 				end
 			end
+			println("For month : ",length(monthVisitors[month]))
 		end
 	end
 end
 
 year = 0
-sync()
+# sync()
 stats = getStats()
 printStats(stats)
