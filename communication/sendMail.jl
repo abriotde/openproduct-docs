4bfa87cdb4b2b1868267899085096513744ae4dc
alberic <alberic.briot@osiris-team.fr>
2025-10-24 09:02:05 +0200

    UPDATE comptes : Mise a jour des comptes avec 2024/2025 UPDATE sendMail : L'envoie de mail pour la nouvelle version du site

--- communication/sendMail.jl
+++ communication/sendMail.jl
@@ -1,30 +1,57 @@
-#!/home/alberic/.cargo/bin/julia
+#!/bin/env julia
 #!/snap/bin/julia
 
 # ADD Header :
 # Date: Sat, 1 Jan 2000 12:00:59 +0200
 
-import SMTPClient, Random, DBInterface, OteraEngine
+import SMTPClient, Random, DBInterface, OteraEngine, TOML
 
 EMAIL_BODY_1ST_COMM_TEMPLATE_FILE = "template1stCommunication.html"
-EMAIL_BODY_AG_TEMPLATE_FILE = "templateMailAG.html"
-SUBJECT = "Promotion des producteurs locaux"
+EMAIL_BODY_TEMPLATE_FILE = "templateMail.html"
 
 
-include("../lib/OpenProduct.jl/src/OpenProduct.jl")
+
+ENV="dev"
+conffile = "../.env.production"
+if isfile(conffile)
+	println("Production environment")
+	ENV="prod"
+else
+	println("Development environment")
+end
+
+
+CURDIR=pwd()
+include_path = ".."
+if ENV=="dev"
+	include_path = "../../openproduct-web-svelte4"
+end
+include(include_path*"/scripts/connect.jl")
+dbConnection = get_connection(include_path)
+
+if ENV=="dev"
+	conffile = include_path*"/.env.local"
+else
+	conffile = include_path*"/.env.production"
+end
+println("Use configuration file : ", conffile)
+conf = TOML.parsefile(conffile)
+SMTP_HOST = conf["SMTP_HOST"]
+SMTP_PORT = conf["SMTP_PORT"]
+SMTP_PASS = conf["SMTP_PASS"]
+SMTP_USER = conf["SMTP_USER"]
+
 # using OpenProduct
-dbConnection = OpenProduct.dbConnect("../../openproduct-web/db/connection.yml")
-OpenProduct.op_start(dbConnection)
 
 function sendMail(to, subject, message)::Bool
-	from = "contact@openproduct.fr"
+	from = SMTP_USER
 	fromC = "OpenProduct <"*from*">"
 	mime_msg = SMTPClient.get_mime_msg(HTML(replace(message, "\n"=>"\r\n")))
 	rcpt = [to]
 	body = SMTPClient.get_body(rcpt, fromC, subject, mime_msg)
 	# println("Body:",String(take!(body)))
-	url = "smtps://mail.openproduct.fr:465"
-	opt = SMTPClient.SendOptions(isSSL=true, username=from, passwd="C{Q~QY\$DJTJ*m[X")
+	url = "smtps://"*SMTP_HOST*":"*string(SMTP_PORT)
+	opt = SMTPClient.SendOptions(isSSL=true, username=SMTP_USER, passwd=SMTP_PASS)
 	resp = SMTPClient.send(url, rcpt, from, body, opt)
 	if resp.code==250
 		true
@@ -39,25 +66,25 @@ function generateToken()::String
 	Random.randstring(64)
 end
 
-function sendMailForAG()
-	sql2 = """UPDATE producer
-		SET nbMailSend=nbMailSend+1, lastEmailSendDate=now()
-		WHERE email=?
+function sendMail2AllProducers(subject, templateFilepath4Body)
+	sql2 = """UPDATE producers
+		SET has_send_mail=true
+		WHERE email=\$1
 		"""
 	sqlUpdateSendEmail = DBInterface.prepare(dbConnection, sql2)
 	sqlUpdateToken = nothing
 
-	templateMailBody = OteraEngine.Template(EMAIL_BODY_AG_TEMPLATE_FILE)
+	templateMailBody = OteraEngine.Template(templateFilepath4Body)
 
-	sql = """SELECT name, coalesce(lastname,firstname,"") lastname, email, tokenAccess
-		FROM producer
+	sql = """SELECT p.company_name, coalesce(lastname, firstname , '') lastname, email, token
+		FROM producers p 
 		WHERE email is not null and email!=''
-			AND (nbMailSend=1) AND sendEmail='Yes'
-			AND status='actif'
+			AND send_email='ok'
+			AND status in('actif', 'hs', 'to-check', 'unknown')
+			AND has_send_mail=false
 		ORDER BY ID
-		LIMIT 0,100
-		"""
-	println("SQL:", sql)
+		LIMIT 10"""
+	println("SQL:", sql);
 	producers = DBInterface.execute(dbConnection, sql)
 	myProducers = []
 	for producer in producers
@@ -72,25 +99,29 @@ function sendMailForAG()
 			println("Generate token for ",email)
 			token = generateToken()
 			if isnothing(sqlUpdateToken)
-				sql2 = """UPDATE producer 
-					SET tokenAccess=?
-					WHERE email=?
+				sql2 = """UPDATE producers 
+					SET token=\$1
+					WHERE email=\$2
 					"""
 				sqlUpdateToken = DBInterface.prepare(dbConnection, sql2)
 			end
 			DBInterface.execute(sqlUpdateToken, [token, email])
 		end
 		dictionary=Dict(
-			"email"=> email,
-			"lastname"=> lastname,
-			"name"=> name,
-			"token"=> token
+			Symbol("email")=> email,
+			Symbol("lastname")=> lastname,
+			Symbol("name")=> name,
+			Symbol("token")=> token
 		)
 		message = templateMailBody(init=dictionary)
-		ok = sendMail(email, SUBJECT, message)
+		slp = rand(1:240)
+		println("Sleep ",slp," s.")
+		sleep(slp) # Sleep between up to 4 minutes.
+		# println("Send mail to ",email,".")
+		ok = sendMail(email, subject, message)
 		if ok
 			DBInterface.execute(sqlUpdateSendEmail, [email])
-			println("sent message to ",email," (",name,", ",lastname,")")
+			println("Sent message to ",email," (",name,", ",lastname,")")
 		else
 			println("Fail sent email")
 		end
@@ -98,16 +129,17 @@ function sendMailForAG()
 end
 
 function sendMailToNewProducers()
-	sql2 = "UPDATE producer SET sendEmail='Yes' WHERE email=?"
+	sql2 = "UPDATE producers SET send_email='Ok' WHERE email=\$1"
 	sqlUpdateSendEmail = DBInterface.prepare(dbConnection, sql2)
 	sqlUpdateToken = nothing
 
 	template1stCommunication = OteraEngine.Template(EMAIL_BODY_1ST_COMM_TEMPLATE_FILE)
+	subject = ""
 
-	sql = """SELECT name, coalesce(lastname,firstname,"") lastname, email, tokenAccess
-		FROM producer 
+	sql = """SELECT name, coalesce(lastname,firstname,"") lastname, email, token
+		FROM producers
 		WHERE email is not null and email!=''
-			AND (sendEmail is null) AND tokenAccess is NULL
+			AND send_email='unknown'
 		ORDER BY ID
 		"""
 	println("SQL:", sql)
@@ -121,22 +153,23 @@ function sendMailToNewProducers()
 			println("Generate token for ",email)
 			token = generateToken()
 			if isnothing(sqlUpdateToken)
-				sql2 = """UPDATE producer 					SET tokenAccess=?
-					WHERE email=?
+				sql2 = """UPDATE producers
+					SET token=\$1
+					WHERE email=\$2
 					"""
 				sqlUpdateToken = DBInterface.prepare(dbConnection, sql2)
 			end
 			DBInterface.execute(sqlUpdateToken, [token, email])
 		end
 		dictionary=Dict(
-			"email"=> email,
-			"lastname"=> lastname,
-			"name"=> name,
-			"token"=> token
+			Symbol("email")=> email,
+			Symbol("lastname")=> lastname,
+			Symbol("name")=> name,
+			Symbol("token")=> token
 		)
 		message = template1stCommunication(init=dictionary)
 		DBInterface.execute(sqlUpdateSendEmail, [email])
-		ok = sendMail(email, SUBJECT, message)
+		ok = sendMail(email, subject, message)
 		if ok
 			println("sent message to ",email," (",name,", ",lastname,")")
 		end
@@ -144,6 +177,10 @@ function sendMailToNewProducers()
 end
 
 # sendMailToNewProducers()
-sendMailForAG()
+# sendMailForAG()
+sendMail2AllProducers(
+	"OpenProduct évolue ! Gérez désormais votre profil en direct",
+	EMAIL_BODY_TEMPLATE_FILE
+)
+# sendMail("alberic.delacrochais@protonmail.com", "Test", "Un petit message de test")
 
-OpenProduct.op_stop(OpenProduct.ok ,dbConnection)
